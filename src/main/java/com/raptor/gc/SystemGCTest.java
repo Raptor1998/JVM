6d8ef9a1a7eefe19349b3a8cc56e2b4394ba82a1
Raptor1998 <knightcwh@163.com>
2022-01-12 20:26:08 +0800

    垃圾回收算法

---
+++ src/main/java/com/raptor/gc/SystemGCTest.java
@@ -0,0 +1,21 @@
+package com.raptor.gc;
+
+/**
+ * @author raptor
+ * @description SystemGCTest
+ * @date 2022/1/12 11:04
+ */
+public class SystemGCTest {
+    public static void main(String[] args) {
+        new SystemGCTest();
+        // 提醒JVM进行垃圾回收
+        System.gc();
+        //System.runFinalization();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("SystemGCTest 执行了 finalize方法");
+    }
+}
