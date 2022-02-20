fafc4b5521a58258c34386b583bba2b5816e4490
Raptor1998 <knightcwh@163.com>
2022-02-20 17:18:27 +0800

    String review

--- src/main/java/com/raptor/jvm/chapter13/NewStringTest.java
+++ src/main/java/com/raptor/jvm/chapter13/NewStringTest.java
@@ -7,10 +7,15 @@ package com.raptor.jvm.chapter13;
  */
 public class NewStringTest {
     public static void main(String[] args) {
-        //String s1 = new String("asd");
+
+        String s1 = "asd";
+
         String s2 = new String("as") + new String("d");
         String s3 = s2.intern();
-        System.out.println(s2==s3);
+
+        System.out.println(s2 == s3);
+        System.out.println(s1 == s3);
+        System.out.println(s1 == s2);
     }
 
 }
